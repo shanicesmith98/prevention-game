7ebd8b1087746cfd6cec3b250979c2783ccf8751
cdalangin <florenzdalangin@gmail.com>
2020-07-17 16:32:09 -0400

    looping background!!!

--- control.js
+++ control.js
@@ -1,36 +1,39 @@
-var can = document.getElementById('canvas1'); 
+var can = document.getElementById('canvas1');
+var ctx = can.getContext('2d');
 
-var ctx = can.getContext('2d'); 
-
-can.width = 1920; 
-can.height = 550; 
-
-var img = new Image(); 
-img.src = "buildings.jpg"; 
+// canvas dimensions
+can.width = 1920;
+can.height = 550;
 
 
-window.onload = function() { 
- 
-	var imgWidth = 0; 
 
-	var scrollSpeed = 3; 
-
-
-	function loop() { 
-		ctx.drawImage(img, imgWidth, 0, can.width, can.height); 
-
-		ctx.drawImage(img, imgWidth - can.width, 0, can.width, can.height); 
-
-		// update image height 
-		imgWidth += scrollSpeed; 
-
-		if (imgWidth == can.width) 
-            imgWidth = 0; 
+//pictures
+var img = new Image();
+img.src = "buildings.jpg";
 
 
-		window.requestAnimationFrame(loop); 
-	} 
+window.onload = function () {
 
-	loop(); 
+	var imgWidth = 0;
 
+	var scrollSpeed = 3;
+
+
+	function loop() {
+		ctx.drawImage(img, imgWidth, 0, can.width, can.height);
+		ctx.drawImage(img, imgWidth + can.width, 0, can.width, can.height);
+
+
+
+		// update image width 
+		imgWidth -= scrollSpeed;
+
+		if (imgWidth == -can.width)
+			imgWidth = 0;
+
+
+		window.requestAnimationFrame(loop);
+	}
+
+	loop();
 } 
