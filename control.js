38a531a3b6ff028876396e68dfa38398589457bc
cdalangin <florenzdalangin@gmail.com>
2020-07-21 14:08:10 -0400

    fixed bg, jumping sprite, title

--- control.js
+++ control.js
@@ -2,32 +2,54 @@ var can = document.getElementById('canvas1');
 var ctx = can.getContext('2d');
 
 // canvas dimensions
-can.width = 1920;
-can.height = 550;
+can.width = 1350;
+can.height = 470;
 
 
-//pictures
+// building source
 var img = new Image();
 img.src = "buildings.jpg";
+// sprite source
+var spriteReady = false;
+var sprite = new Image();
+sprite.src = "sprite3.png";
+
+// keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
 
 
+// drawing the canvas
 window.onload = function () {
-
-	var imgWidth = 0;
-
 	var scrollSpeed = 3;
-
+	var imgXPos = 0;
 
 	function loop() {
-		ctx.drawImage(img, imgWidth, 0, can.width, can.height);
+		//looping background
+		ctx.drawImage(img, imgXPos, 0, can.width, can.height);
 
-		ctx.drawImage(img, imgWidth + can.width, 0, can.width, can.height);
+		ctx.drawImage(img, imgXPos + can.width, 0, can.width, can.height);
 
-		// update image width 
-		imgWidth -= scrollSpeed;
+		imgXPos -= scrollSpeed;
 
-		if (imgWidth == -can.width)
-			imgWidth = 0;
+		if (imgXPos == -can.width)
+			imgXPos = 0;
+
+		// sprite 
+		if (32 in keysDown == false) {
+			ctx.drawImage(sprite, 40, 290, 120, 160)
+		}
+		if (32 in keysDown) {
+			ctx.drawImage(sprite, 40, 220, 120, 160)
+		}
+
 
 		window.requestAnimationFrame(loop);
 	}
